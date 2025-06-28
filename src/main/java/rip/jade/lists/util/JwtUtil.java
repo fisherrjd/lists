a595d736f7ef6b8a86b8256fe8c59c297e755d61
jade fisher <fisherrjd@gmail.com>
2025-06-28 13:56:38 -0600

    abstract secrets and change them

--- src/main/java/rip/jade/lists/util/JwtUtil.java
+++ src/main/java/rip/jade/lists/util/JwtUtil.java
@@ -4,14 +4,16 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-    // TODO Hide this
-    // Use a secure key of at least 256 bits (32 bytes) for HS256
-    private final String SECRET_KEY = "replace_this_with_a_very_long_and_random_secret_key_32bytes_min";
+
+    @Value("${JWT_SECRET}")
+    private String SECRET_KEY;
+
     private final long EXPIRATION_TIME = 1000 * 60 * 60 * 10; // 10 hours
 
     public String generateToken(String username) {
