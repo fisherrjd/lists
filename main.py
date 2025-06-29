d90540c2f920e595372a6f4244f65899d3cc9d03
jade fisher <fisherrjd@gmail.com>
2025-06-29 13:40:30 -0600

    init configs

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from typing import Union
-
 from fastapi import FastAPI
+import uvicorn
+from core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.app_name, debug=settings.debug)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=settings.server_port, reload=True)
